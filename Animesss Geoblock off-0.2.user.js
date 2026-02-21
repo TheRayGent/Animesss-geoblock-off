2fd70bd715dffd600c07139a496eab653f6f6c01
TheRayGent <109617962+TheRayGent@users.noreply.github.com>
2026-02-21 15:27:32 +0200

    fix

--- Animesss Geoblock off-0.2.user.js
+++ Animesss Geoblock off-0.2.user.js
@@ -3,11 +3,11 @@
 // @match        https://animesss.com/aniserials/video/*
 // @match        https://animesss.tv/aniserials/video/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=animesss.com
-// @version      0.2
+// @version      0.2.1
 // ==/UserScript==
 
 (function() {
     'use strict';
 
-    document.getElementById('player_kodik').innerHTML = document.getElementById('player_kodik').innerHTML.replace(/geoblock=/g, '');
+    document.getElementById('kodik_player_ajax').innerHTML = document.getElementById('kodik_player_ajax').innerHTML.replace(/geoblock=/g, '');
 })();
