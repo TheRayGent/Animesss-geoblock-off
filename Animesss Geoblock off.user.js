4d1daac1a49b080eb0966fae9a0618d509b49c4f
TheRayGent <109617962+TheRayGent@users.noreply.github.com>
2026-07-19 18:47:01 +0200

    Update Animesss Geoblock off.user.js

--- Animesss Geoblock off.user.js
+++ Animesss Geoblock off.user.js
@@ -5,15 +5,15 @@
 // @match        https://animesss.com/ova/*
 // @match        https://animesss.tv/ova/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=animesss.com
-// @version      0.3.0
+// @version      0.4.0
 // ==/UserScript==
 
 (function() {
     'use strict';
 
     function geoblock_off() {
-        const player = document.getElementById('player');
-        const list = document.getElementById('translators-list');
+        const player = document.getElementById('player_kodik');
+        const list = document.querySelector('.b-translators__list[data-donor="kodik"]');
 
         if (player) {
             player.innerHTML = player.innerHTML.replace(/geoblock=/g, '');
